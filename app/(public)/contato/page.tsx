64cd90187375ce875288e11dd88f77f77c72486c
Nathan Souza <nathangabi2021-commits>
2026-02-19 12:52:56 -0300

    versaoatt

---
+++ app/(public)/contato/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import localhotel from "./local.png"
+import Image from "next/image";
+export default function Contato(){
+    return (
+        <section className="flex flex-col m-6  ...">
+                <Link href="/home"><h1 className="text-center ">Quem Somos</h1></Link> 
+
+              <div className="flex  w-full p-8 gap-15   ...">
+                 <span>Construído em uma das principais avenidas de Jaú, o Realce Hotel está localizado estrategicamente na entrada da cidade, facilitando o acesso dos seus hóspedes para diferentes locais na sua estadia.
+                 
+                <br />  <br /> 
+           
+
+                Sua infraestrutura versátil e sua equipe extremante atenciosa e bem treinada faz do Realce Hotel a sua casa em Jaú! Seja a negócios ou lazer.
+
+                 <br />  <br /> 
+
+                Você vai realmente sentir-se em casa ao se hospedar em nossos quartos que possuem TV a cabo, ar condicionado e wi-fi. Além disso, fique à vontade para começar o dia com nosso delicioso café da manhã e se exercitar em nossa academia.
+
+                 <br />  <br /> 
+
+                O Realce Hotel oferece uma sala de reuniões confortável e bem equipada, com capacidade para até 30 pessoas. Ideal para encontros corporativos e eventos empresariais, o espaço proporciona um ambiente profissional e funcional para reuniões produtivas.</span>
+                
+                  <div className=" flex justify-center  ...">
+                 <Image className="w-400" src={localhotel} alt="localização do hotel" width={400} height={400}/>
+                 </div>
+              </div>
+               
+        </section>
+
+
+    )
+}
--- app/(public)/home/page.tsx
+++ app/(public)/home/page.tsx
@@ -1,35 +1,10 @@
 import Image from "next/image";
-import realceimage from "./realcehotel.png"
-import imagens from "./app.png"
-import opcoes from "./options.png"
 import apresentacao from "./apresent.png"
 import eventss from "./events.png"
-import ImagesFlex from "@/app/(components)/imagesgrid/page";
-import Footer from "@/app/(components)/footer/page";
-import Link from "next/link";
 import Carousel from "@/app/(components)/carousel/page";
 export default function Home(){
     return(
         <main >
-                <header className="bg-zinc-800 flex justify-center ">
-                    <Image className="bg-white-200" src={realceimage}  alt="logo da pagina" width={300} 
-                    height={100}/>
-                    <nav  >
-                        <ul className="flex  gap-5 p-20 text-white">
-                            <li><button className="bg-zinc-500 hover:bg-sky-700 ... w-20 h-10">Reservas</button></li>
-                            <li><button className="bg-zinc-500 hover:bg-sky-700 ... w-20 h-10">Check-in</button></li>
-                        </ul>
-                    </nav>
-                </header>
-                 <nav className="p-5 bg-zinc-700 ...">
-                    <ul className="flex justify-evenly text-white ...">
-                        <li><Link href="/register"><button className="bg-white-600 hover:bg-sky-500 ... w-20 h-10 ">Home</button></Link></li>
-                        <li><button className="bg-white-600 hover:bg-sky-500 ... w-30 h-10">Quem Somos</button></li>
-                        <li><button className="bg-white-600 hover:bg-sky-500 ... w-30 h-10">Acomodações</button></li>
-                        <li><button className="bg-white-600 hover:bg-sky-500 ... w-20 h-10">Fotos</button></li>
-                        <li><button className="bg-white-600 hover:bg-sky-500 ... w-20 h-10">Contato</button></li>
-                    </ul>
-                 </nav> 
 
                  <div>
                         <Carousel/>
@@ -66,11 +41,10 @@ export default function Home(){
                 <div className="flex-row p-5 justify-items-center ...">
                     <h1 className="text-center p-4">Localização</h1>
 
-                    <iframe  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14764.548921328484!2d-48.588616738623045!3d-22.310649208049323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1771163765150!5m2!1spt-BR!2sbr" width="660" height="450"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14764.548921328484!2d-48.588616738623045!3d-22.310649208049323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1771163765150!5m2!1spt-BR!2sbr" width="660" height="450"    loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div> 
-
-
-               <Footer/>
+ 
+   
         </main>
     )
 }
