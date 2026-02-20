860a2c08aa1a494e20018f29d1bd4b3de5553c92
Nathan Souza <nathangabi2021-commits>
2026-02-20 11:33:04 -0300

    mvpatualizadaandamento

--- app/(components)/carousel/page.tsx
+++ app/(components)/carousel/page.tsx
@@ -9,23 +9,41 @@ import recepcafe from "./recepcaocafe.png"
 import cafemanhaa from "./cafemanha.png"
 import hotelfrente from "./frente.png"
 import Image from 'next/image';
-export default function Carousel(){
-    return(
+ 
+import { useState } from 'react';
+export default function Carousel(){ 
+    const [isReady, setIsReady] = useState(false);    
 
-        <Swiper
+
+    let carroselexiste = true
+       carroselexiste = carroselexiste ? <h1 className='text-2xl text-center font-bold mb-4'>  
+         <Swiper
             modules={[Navigation, Pagination]}
             spaceBetween={50}
             slidesPerView={1}
+            onSwiper={() => setIsReady(true)}
+            className={isReady ? 'opacity-100' : 'opacity-0'}
+            style={{transition: 'opacity 0.3s ease'}}
+            observeParents={true}
+            observer={true}
             navigation
             pagination={{clickable: true}}
+
     
         >
-            <SwiperSlide><Image src={salareuniaohotel} alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
-            <SwiperSlide><Image src={recepcafe}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
-            <SwiperSlide><Image src={cafemanhaa}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
-            <SwiperSlide><Image src={hotelfrente}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
+            <SwiperSlide><Image fetchPriority="high" loading='lazy' src={salareuniaohotel} alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
+            <SwiperSlide><Image fetchPriority="high" loading='lazy' src={recepcafe}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
+            <SwiperSlide><Image fetchPriority="high" loading='lazy' src={cafemanhaa}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
+            <SwiperSlide><Image fetchPriority="high" loading='lazy' src={hotelfrente}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
 
-        </Swiper>
-        
+        </Swiper>  </h1> : <h1 className='text-2xl text-center font-bold mb-4'>Nenhuma foto disponível</h1>
+    return(
+ 
+          <section> 
+
+            {carroselexiste ? carroselexiste : <p>Nenhuma foto disponível</p>}
+
+          </section>
+
     )
 }
