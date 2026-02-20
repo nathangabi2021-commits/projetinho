66b33e4f9e269eddaac58b7e805e8db8094c1abe
Nathan Souza <nathangabi2021-commits>
2026-02-20 10:52:31 -0300

    mvpatualizadosucces

--- app/(public)/fotos/page.tsx
+++ app/(public)/fotos/page.tsx
@@ -8,6 +8,14 @@ import confortox from "./conforto.png"
 import localizados from "./localizado.png"
 import localizacao from "./grupoviagens.png"
 import recepcao from "./recepcao.png"
+import recepcaonova from "./recepcaonova.png"
+import espaconovo from "./poltronasespaco.png"
+import cafemanha from "./cafe.png"
+import cafebolos from "./cafebolos.png"
+import paes from "./paes.png"
+import academia from "./academia.png"
+import reuniao from "./reuniao.png"
+import entrada from "./entrada.png"
 export default function Fotos(){
     return(
          <div className="grid grid-cols-4 gap-4">
@@ -19,6 +27,16 @@ export default function Fotos(){
                 <div><Image src={localizados} alt="acomodacao 1" width={300} height={200}/></div>
                 <div><Image src={localizacao} alt="acomodacao 1" width={300} height={200}/></div>
                 <div><Image src={recepcao} alt="acomodacao 1" width={300} height={200}/></div>
+
+                <div><Image src={recepcaonova} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image className="h-55" src={espaconovo} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image src={cafemanha} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image src={cafebolos} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image src={paes} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image src={academia} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image src={reuniao} alt="acomodacao 1" width={300} height={200}/></div>
+                <div><Image src={entrada} alt="acomodacao 1" width={300} height={200}/></div>
+
          </div>
     )
 }
