8ba1ef7532ac3dff456f5ab7f5c8899a3d5671c7
Nathan Souza <nathangabi2021-commits>
2026-02-21 13:51:47 -0300

    testing

--- app/(components)/carousel/page.tsx
+++ app/(components)/carousel/page.tsx
@@ -10,13 +10,20 @@ import recepcafe from "./recepcaocafe.png"
 import cafemanhaa from "./cafemanha.png"
 import hotelfrente from "./frente.png"
 import Image from 'next/image';
+
+type CarouselProps = {
+    carrosel: boolean;
+}
  
+
 import { useState } from 'react';
 export default function Carousel(){ 
     const [isReady, setIsReady] = useState(false);    
-
-
-         <Swiper
+        
+    const carouselConfig: CarouselProps = {carrosel: true}; // Defina como true para exibir o carrossel
+    const carrosel = carouselConfig.carrosel ? (
+        
+        <Swiper
             modules={[Navigation, Pagination]}
             spaceBetween={50}
             slidesPerView={1}
@@ -35,11 +42,13 @@ export default function Carousel(){
             <SwiperSlide><Image className="object-cover fill priority" src={cafemanhaa}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
             <SwiperSlide><Image className="object-cover fill priority" src={hotelfrente}  alt="fotoshotel" width={1350} height={1350}></Image></SwiperSlide>
 
-        </Swiper> 
+        </Swiper>    ) : console.log("Carrossel não está pronto")
+  
+        
     return(
  
           <section> 
- 
+                 {carrosel ? carrosel : <p>Carrossel não está pronto</p>}
           </section>
 
     )
